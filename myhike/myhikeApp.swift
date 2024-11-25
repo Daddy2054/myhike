6be0f46767f9b626d9ce1288785ae52ecb5fa4ec
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-11-25 22:12:47 +0100

    Initial Commit

---
+++ myhike/myhikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  myhikeApp.swift
+//  myhike
+//
+//  Created by Jean on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct myhikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
