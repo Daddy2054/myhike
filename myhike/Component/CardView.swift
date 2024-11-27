ff205edf1410ff4657bb6f1a076b8eb50067f7fc
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-11-27 23:49:11 +0100

    30. 6. SwiftUI Sheet

--- myhike/Component/CardView.swift
+++ myhike/Component/CardView.swift
@@ -11,6 +11,7 @@ struct CardView: View {
     //  mark: properties
     @State private var imageNumber:  Int = 1
     @State private var randomNumber:  Int = 1
+    @State private var isShowingSheet: Bool = false
     
     // functions
     func randomImage() {
@@ -36,11 +37,15 @@ struct CardView: View {
                         Button
                         {// action show a sheeet
                             print("show sheet")
+                            isShowingSheet.toggle()
                         }
                         label: {
                             CustomButtonView()
-                            
-                            
+                        }
+                        .sheet(isPresented: $isShowingSheet ){
+                            SettingsView()
+                                .presentationDragIndicator(.visible)
+                                .presentationDetents([.medium, .large])
                         }
                     }
                     Text("Fun  and enjoyable outdoor activities for all ages")
